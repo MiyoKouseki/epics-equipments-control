e2b8424a5f8c709577285eb68ce67fc3661cd473
miyo <miyo@gwdhcp087.icrr.u-tokyo.ac.jp>
2018-02-22 09:20:03 +0900

    ディレクトリの構造を整理した。

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-this is for adl MIYOPICO_ver1.adl.
-"""
-import logging
-def get_module_logger(modname):
-    logging.config.fileConfig('logging.conf')
-    logger = logging.getLogger('logExample')
-    return logger
-
